126faf1cd243c9b027107bbd64a6632242f6ba16
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-06-07 14:15:31 +0300

    June-2023

--- 1 _ C-sharp/6 _ Field & Constant/6 _ Field & Constant/Program.cs
+++ 1 _ C-sharp/6 _ Field & Constant/6 _ Field & Constant/Program.cs
@@ -6,10 +6,17 @@ namespace Casting
     {
         static void Main(string[] args)
         {
-            // Object Syntax 
+            /*
+             بالستاك تخزن الأوبجكت وبالهيب تخزنت القيمة تعت الأوبجكت
+             object stored in stack , value of obj stored in heap
+             */
+            // Object (Instance) Syntax 
             // Declaration <Type> <ObjectName>;
             // Assignment <ObjectName> = new <Type>();
             // Initialization <Type> <ObjectName> = new <Type>();
+
+            Console.WriteLine("First Employee\n");
+
             Employee employee = new Employee();
 
             Console.Write("Enter Your First Name: ");
@@ -24,13 +31,46 @@ namespace Casting
             Console.Write("Enter the Logged Hours : ");
             employee.LoggedHours = Convert.ToDouble(Console.ReadLine());
 
-            var netSalary = employee.Wage * employee.LoggedHours - (employee.Wage * employee.LoggedHours * Employee.TAX);
+            //var netSalary = employee.Wage * employee.LoggedHours - (employee.Wage * employee.LoggedHours * Employee.TAX);
 
-            Console.WriteLine($"First Name : {employee.FName}");
-            Console.WriteLine($"Last Name : {employee.LName}");
-            Console.WriteLine($"Wage : {employee.Wage}");
-            Console.WriteLine($"logged hours : {employee.LoggedHours}");
-            Console.WriteLine($"net salary : {netSalary}");
+            //Console.WriteLine($"First Name : {employee.FName}");
+            //Console.WriteLine($"Last Name : {employee.LName}");
+            //Console.WriteLine($"Wage : {employee.Wage}");
+            //Console.WriteLine($"logged hours : {employee.LoggedHours}");
+            //Console.WriteLine($"net salary : {netSalary}");
+
+            Employee[] employees = new Employee[2];
+
+            employees[0] = employee;
+
+
+            Console.WriteLine("\nSecond Employee");
+
+            Employee employee2 = new Employee();
+
+            Console.Write("Enter Your First Name: ");
+            employee2.FName = Console.ReadLine();
+
+            Console.Write("Enter Your Last Name: ");
+            employee2.LName = Console.ReadLine();
+
+            Console.Write("Enter the Wage : ");
+            employee2.Wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Logged Hours : ");
+            employee2.LoggedHours = Convert.ToDouble(Console.ReadLine());
+
+            employees[1] = employee2;
+
+            foreach (var employee1 in employees)
+            {
+                var netSalary = employee1.Wage * employee1.LoggedHours - (employee1.Wage * employee1.LoggedHours * Employee.TAX);
+                Console.WriteLine($"First Name : {employee1.FName}");
+                Console.WriteLine($"Last Name : {employee1.LName}");
+                Console.WriteLine($"Wage : {employee1.Wage}");
+                Console.WriteLine($"logged hours : {employee1.LoggedHours}");
+                Console.WriteLine($"net salary : {netSalary}");
+            }
         }
     }
 }
