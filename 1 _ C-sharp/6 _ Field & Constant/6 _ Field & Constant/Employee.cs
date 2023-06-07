8264d4db4688406b8d8b8175299a8b3225e19d10
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-06-07 10:15:01 +0300

    June-2023

---
+++ 1 _ C-sharp/6 _ Field & Constant/6 _ Field & Constant/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace _6___Field___Constant
+{
+    //<ClassModifier> -> public, internal(default) متاح ضمن قيود معينة
+    public class Employee //<ClassModifier> class <ClassName>
+    {
+        // <accessModifier> public, private, protected
+        // Constant  -> <AccessModifier> const <DataType> <ConstantName> = <Value>;
+        public const double TAX = 0.03;
+        // <accessModifier> public, private, protected
+        // Fields  -> <AccessModifier> <DataType> <FieldName> = <InitialValue>;
+        public string? FName;
+        public string? LName;
+        public double Wage;
+        public double LoggedHours;
+    }
+}
---
+++ 1 _ C-sharp/6 _ Field & Constant/6 _ Field & Constant/Program.cs
@@ -0,0 +1,37 @@
+﻿using _6___Field___Constant;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object Syntax 
+            // Declaration <Type> <ObjectName>;
+            // Assignment <ObjectName> = new <Type>();
+            // Initialization <Type> <ObjectName> = new <Type>();
+            Employee employee = new Employee();
+
+            Console.Write("Enter Your First Name: ");
+            employee.FName = Console.ReadLine();
+
+            Console.Write("Enter Your Last Name: ");
+            employee.LName = Console.ReadLine();
+
+            Console.Write("Enter the Wage : ");
+            employee.Wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Logged Hours : ");
+            employee.LoggedHours = Convert.ToDouble(Console.ReadLine());
+
+            var netSalary = employee.Wage * employee.LoggedHours - (employee.Wage * employee.LoggedHours * Employee.TAX);
+
+            Console.WriteLine($"First Name : {employee.FName}");
+            Console.WriteLine($"Last Name : {employee.LName}");
+            Console.WriteLine($"Wage : {employee.Wage}");
+            Console.WriteLine($"logged hours : {employee.LoggedHours}");
+            Console.WriteLine($"net salary : {netSalary}");
+        }
+    }
+}
+
