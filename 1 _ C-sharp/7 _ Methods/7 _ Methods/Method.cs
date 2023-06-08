2eab51ebf26a576f150edd885222d09dcfb532ef
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-06-08 15:58:25 +0300

    June-2023

--- 1 _ C-sharp/7 _ Methods/7 _ Methods/Method.cs
+++ 1 _ C-sharp/7 _ Methods/7 _ Methods/Method.cs
@@ -6,7 +6,18 @@
         {
             Demo demo = new Demo();
 
-            demo.DoSomething(); // Caller إسمه
+            demo.DoSomething(); // Caller إسمه - void
+
+            demo.ReturnInteger(); // primary expression
+
+            var z = 100 + demo.ReturnInteger();
+            Console.WriteLine(z);
+
+            var Age = 19;
+            demo.CalculateAge(Age); // 18 is the argument passed
+
+            demo.DoAnything(ref Age);
+            Console.WriteLine(Age);
         }
     }
 
@@ -21,6 +32,25 @@
         public void DoSomething() // Callee إسمه
         {
             Console.WriteLine("Do Something...");
+        }
+
+        public int ReturnInteger()
+        {
+            var x = 100;
+            return x * x;
+        }
+
+        public void CalculateAge(int Age) // Age is parameter
+        {
+            // Age بتنبعت by value
+            Age = Age + 3;
+            Console.WriteLine(Age);
+        }
+
+        public void DoAnything(ref int Age) // Age is parameter
+        {
+            // Age بتنبعت by Reference
+            Age = Age + 3;
         }
     }
 }
