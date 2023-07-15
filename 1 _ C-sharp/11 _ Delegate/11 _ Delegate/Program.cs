4aa9eb35c44dd96203268600eeb3a4cf54ed9fc1
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-07-15 09:19:13 +0300

    July-2023

--- 1 _ C-sharp/11 _ Delegate/11 _ Delegate/Program.cs
+++ 1 _ C-sharp/11 _ Delegate/11 _ Delegate/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace _11___Delegate
 {
+    public delegate void RectDelegete(decimal Height, decimal Width); // Multicast Delegate
     class Program
     {
         static void Main(string[] args)
@@ -13,18 +14,43 @@
                 new Employee {Id = 5, Name = "Salah C", Gender = "m", TotalSales = 30000m},
                 new Employee {Id = 6, Name = "Rateb A", Gender = "M", TotalSales = 50000m },
                 new Employee {Id = 7, Name = "Abeer C", Gender = "F", TotalSales = 16000m },
-                new Employee {Id = 8, Name = "Marwan м", Gender = "M", TotalSales = 15000m },
+                new Employee {Id = 8, Name = "Marwan M", Gender = "M", TotalSales = 15000m },
             };
 
             var report = new Report();
-            report.ProcessEmployeeWith60000PlusSales(emp);
-            report.ProcessEmployeeWithSalesBetween30000And59999(emp);
-            report.ProcessEmployeeWithSalesLessThan30000(emp);
+            report.ProcessEmployee(emp, "Sales >= 60,000", delegate (Employee emp) { return emp.TotalSales > 60000m; }); // delegate بس بطريقة أسهل من الطريقة الأخيرة - إلي بعدها بسطرين
+            report.ProcessEmployee(emp, "Sales <= 30,000 && sales <= 59999", (Employee emp) => emp.TotalSales < 60000m && emp.TotalSales >= 30000m); // delegate with lambda expression
+            report.ProcessEmployee(emp, "Sales < 30000", IsLessThan30000); // delegate بالطريقة العادية
+
+            var helper = new RectangleHelper();
+            RectDelegete rect = helper.GetArea; // Multicast Delegate
+            rect += helper.GetPerimeter;
+            rect(10, 10);
+            Console.WriteLine("After Unsubscribe GetPermeter : ");
+            rect -= helper.GetPerimeter;
+            rect(10, 10);
+
+
+        }
+        static bool IsGreaterThanOrEqual60000(Employee emp) => emp.TotalSales > 60000m;
+        static bool IsBetween30000And59999(Employee emp) => emp.TotalSales < 60000m && emp.TotalSales >= 30000m;
+        static bool IsLessThan30000(Employee emp) => emp.TotalSales < 30000m;
+
+    }
+
+    public class RectangleHelper
+    {
+        // Multicast Delegate عندي عمليتين أو أكثر بدي إياهم يتنفذوا بس ما بدي كل شوي أنادي كل وحدة فيهم ، مرة وحدة بتستدعيهم كلهم
+        public void GetArea(decimal Height, decimal Width)
+        {
+            var result = Height * Width;
+            Console.WriteLine($"{Height} * {Width} = {result}");
+        }
+        public void GetPerimeter(decimal Height, decimal Width)
+        {
+            var result = 2 * (Height + Width);
+            Console.WriteLine($"2 * ({Height} + {Width}) = {result}");
         }
     }
 }
-
-
-
-
 
--- 1 _ C-sharp/11 _ Delegate/11 _ Delegate/Report.cs
+++ 1 _ C-sharp/11 _ Delegate/11 _ Delegate/Report.cs
@@ -3,14 +3,15 @@
     public class Report
     {
         // Delegate (condition)هو الشخص القادر على تغيير الشرط
-        public void ProcessEmployeeWith60000PlusSales(Employee[] employees)
+        public delegate bool IllegibleSales(Employee employee);
+        public void ProcessEmployee(Employee[] employees, string title, IllegibleSales isIllegible)
         {
-            Console.WriteLine("Employees With $60,000+ Sales");
+            Console.WriteLine(title);
             Console.WriteLine("_______________________________");
 
             foreach (Employee employee in employees)
             {
-                if (employee.TotalSales >= 60000m)
+                if (isIllegible(employee))
                 {
                     Console.WriteLine($"{employee.Id} | {employee.Name} | {employee.Gender} | ${employee.TotalSales}");
                 }
@@ -18,34 +19,5 @@
             Console.WriteLine("\n\n");
         }
 
-        public void ProcessEmployeeWithSalesBetween30000And59999(Employee[] employees)
-        {
-            Console.WriteLine("Employees With Sales between $30,000 and $59,000 Sales");
-            Console.WriteLine("_______________________________");
-
-            foreach (Employee employee in employees)
-            {
-                if (employee.TotalSales < 60000m && employee.TotalSales >= 30000m)
-                {
-                    Console.WriteLine($"{employee.Id} | {employee.Name} | {employee.Gender} | ${employee.TotalSales}");
-                }
-            }
-            Console.WriteLine("\n\n");
-        }
-
-        public void ProcessEmployeeWithSalesLessThan30000(Employee[] employees)
-        {
-            Console.WriteLine("Employees With Less Than $30,000+ Sales");
-            Console.WriteLine("_______________________________");
-
-            foreach (Employee employee in employees)
-            {
-                if (employee.TotalSales < 30000m)
-                {
-                    Console.WriteLine($"{employee.Id} | {employee.Name} | {employee.Gender} | ${employee.TotalSales}");
-                }
-            }
-            Console.WriteLine("\n\n");
-        }
     }
 }
