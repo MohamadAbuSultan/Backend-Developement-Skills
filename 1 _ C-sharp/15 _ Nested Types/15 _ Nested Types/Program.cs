dc40cb30baa17aed020c03453673091827e9d2a3
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-08-28 11:01:28 +0300

    August-2023

---
+++ 1 _ C-sharp/15 _ Nested Types/15 _ Nested Types/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NestedTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HW");
+        }
+    }
+    // nested type : type defined within a Class نوع بيانات مخصص يتم تعريفه داخل الكلاس
+
+}
